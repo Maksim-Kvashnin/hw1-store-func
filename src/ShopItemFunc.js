9741beb5b0ed07d415db4a0869a96add81c55745
Maksim_KA <Kvashnin@hotmail.com>
2024-04-29 23:31:34 +0700

    fix props

--- src/ShopItemFunc.js
+++ src/ShopItemFunc.js
@@ -1,17 +1,16 @@
 import React from 'react'
 
-export function ShopItemFunc(props) {
-  const data = props.item;
+export const ShopItemFunc = ({ item }) => {
   return (
     <div className="main-content">
-      <h2>{data.brand}</h2>
-      <h1>{data.title}</h1>
-      <h3>{data.description}</h3>
-      <div className="description">{data.descriptionFull}</div>
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div className="description">{item.descriptionFull}</div>
       <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
       <div className="divider"></div>
       <div className="purchase-info">
-        <div className="price">{data.currence}{(data.price).toFixed(2)}</div>
+        <div className="price">{item.currence}{(item.price).toFixed(2)}</div>
         <button>Добавить в корзину</button>
       </div>
     </div>
